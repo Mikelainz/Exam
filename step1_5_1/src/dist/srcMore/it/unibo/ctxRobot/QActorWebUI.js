0c690200469fcc103a1cd51a74415d3e66df3d44
Mikelainz <mikelainz93@gmail.com>
2017-08-25 19:31:12 +0200

    towards step 2

--- step1_5_1/src/dist/srcMore/it/unibo/ctxRobot/QActorWebUI.js
+++ step1_5_1/src/dist/srcMore/it/unibo/ctxRobot/QActorWebUI.js
@@ -1,33 +1,30 @@
 //QActorWebUI.js
-var curSpeed = "high";
+var curSpeed = "low";
 	
-console.log("QActorWebUI.js : server IP= "+document.location.host);
+    console.log("QActorWebUI.js : server IP= "+document.location.host);
  /*
  * WEBSOCKET
  */
     var sock = new WebSocket("ws://"+document.location.host, "protocolOne");
     sock.onopen = function (event) {
          //console.log("QActorWebUI.js : I am connected to server.....");
-         //document.getElementById("connection").value = 'CONNECTED';
+         document.getElementById("connection").value = 'CONNECTED';
     };
-	
     sock.onmessage = function (event) {
         //console.log("QActorWebUI.js : "+event.data);
         //alert( "onmessage " + event);
-        //document.getElementById("state").value = ""+event.data;
+        document.getElementById("state").value = ""+event.data;
     }
-	
     sock.onerror = function (error) {
         //console.log('WebSocket Error %0',  error);
-        //document.getElementById("state").value = ""+error;
+        document.getElementById("state").value = ""+error;
     };
     
-//	function setSpeed(val){
-//		curSpeed = val;
-//		document.getElementById("speed").value = curSpeed;
-//	}
-	
+	function setSpeed(val){
+		curSpeed = val;
+		document.getElementById("speed").value = curSpeed;
+	}
 	function send(message) {
-		//document.getElementById("sending").value = ""+message;
+		document.getElementById("sending").value = ""+message;
 		sock.send(message);
 	};
